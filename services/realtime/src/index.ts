dc7548c504828148e0769c0a23fb4daf79066663
mohammed Alsakkaf <mohammed.alsakkaaf@gmail.com>
2026-07-22 05:38:39 +0800

    feat(realtime): wire dispatcher startup into index.ts

--- services/realtime/src/index.ts
+++ services/realtime/src/index.ts
@@ -1,6 +1,11 @@
+import { createListenerClient } from "./db.js";
+import { startDispatcher } from "./dispatcher.js";
 import { buildServer } from "./server.js";
 
 const port = Number(process.env.REALTIME_PORT ?? 4001);
-const { server } = buildServer();
+const { server, broadcast } = buildServer();
 
 server.listen(port, "0.0.0.0");
+
+const listenerClient = await createListenerClient();
+startDispatcher(listenerClient, broadcast);
