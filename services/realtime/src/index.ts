a497372ab4be530b1d0776fa14ef93bdffa856fe
mohammed Alsakkaf <mohammed.alsakkaaf@gmail.com>
2026-07-22 14:19:16 +0800

    feat(realtime): log server startup via structured logger (TC-PC10-001)

--- services/realtime/src/index.ts
+++ services/realtime/src/index.ts
@@ -1,6 +1,7 @@
 import { registerWelcomeNotificationConsumer } from "./consumers/welcomeNotification.js";
 import { createListenerClient } from "./db.js";
 import { startDispatcher } from "./dispatcher.js";
+import { logger } from "./logger.js";
 import { buildServer } from "./server.js";
 
 const port = Number(process.env.REALTIME_PORT ?? 4001);
@@ -8,7 +9,9 @@ const { server, broadcast, broadcastToChannel } = buildServer();
 
 registerWelcomeNotificationConsumer(broadcastToChannel);
 
-server.listen(port, "0.0.0.0");
+server.listen(port, "0.0.0.0", () => {
+  logger.info({ port }, "realtime server listening");
+});
 
 const listenerClient = await createListenerClient();
 startDispatcher(listenerClient, broadcast);
